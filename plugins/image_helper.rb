cbd56c83f11f7dff46637f5e04cd4cd44cdd8ef6
Tatsuya Koyama <info@tatsuya-koyama.com>
2015-02-12 03:54:50 +0900

    Add Post

--- plugins/image_helper.rb
+++ plugins/image_helper.rb
@@ -2,7 +2,7 @@ module ImageHelper
   def image(img_path)
     img_path = img_path.strip
     result_html  = "<div class='image-autofix'>\n"
-    result_html += "  <img src='{{urls.production}}{{urls.media}}/#{img_path}'>\n"
+    result_html += "  <img src='{{urls.media}}/#{img_path}'>\n"
     result_html += "</div>"
 
     result_html
